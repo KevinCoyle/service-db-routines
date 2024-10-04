7cf8fd0131be560bdb7bbc6dee79ca4f11c992e4
nomiblek <kevin.coyle@deshittify.tech>
2024-10-04 12:46:27 -0700

    - Initial Setup

---
+++ src/Routines.Api/Migrations/20241004175747_InitialSetup.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Routines.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Routines",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    FollowUpRoutineId = table.Column<Guid>(type: "uuid", nullable: true),
+                    OwnerId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Routines", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Routines_Routines_FollowUpRoutineId",
+                        column: x => x.FollowUpRoutineId,
+                        principalTable: "Routines",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Routines_Users_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Actions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    RoutineId = table.Column<Guid>(type: "uuid", nullable: false),
+                    FollowUpActionId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Actions_Routines_RoutineId",
+                        column: x => x.RoutineId,
+                        principalTable: "Routines",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Schedules",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    RoutineId = table.Column<Guid>(type: "uuid", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Intervals = table.Column<int[]>(type: "integer[]", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedules", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Schedules_Routines_RoutineId",
+                        column: x => x.RoutineId,
+                        principalTable: "Routines",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Actions_RoutineId",
+                table: "Actions",
+                column: "RoutineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Routines_FollowUpRoutineId",
+                table: "Routines",
+                column: "FollowUpRoutineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Routines_OwnerId",
+                table: "Routines",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_RoutineId",
+                table: "Schedules",
+                column: "RoutineId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Actions");
+
+            migrationBuilder.DropTable(
+                name: "Schedules");
+
+            migrationBuilder.DropTable(
+                name: "Routines");
+        }
+    }
+}
